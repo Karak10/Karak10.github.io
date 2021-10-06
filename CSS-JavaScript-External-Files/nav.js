b82e5308c79807e5c3eb072ab2ab51006922103b
Karak10 <alexisgiafis@gmail.com>
2021-10-06 20:30:25 +0300

    Added a new page for my computer science notes, updated nav.js, my diary and extranotes2

--- CSS-JavaScript-External-Files/nav.js
+++ CSS-JavaScript-External-Files/nav.js
@@ -327,7 +327,7 @@ window.addEventListener('load', (e) => {
 
         // loop that creates links
 
-        let pages = 1;
+        let pages = 2;
         let num = 1;
 
         for(num; num <= pages; num++){
